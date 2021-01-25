97b1d4dc456cf71bc42f258f4a5d0b150b796681
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2021-01-25 14:38:36 -0800

    Update Terrain
    - Add new TerrainGenerator

---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Terrain
+{
+    public class TerrainGenerator
+    {
+        private Mesh _mesh;
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void GenerateVertices()
+        {
+
+        }
+
+        public void GenerateIndices()
+        {
+
+        }
+
+        public void GenerateMeshes()
+        {
+
+        }
+
+    }
+
+}
