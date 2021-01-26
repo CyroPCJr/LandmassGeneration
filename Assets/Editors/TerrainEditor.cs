f7d5e04b626a9238241da6983155543c2b51385e
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2021-01-26 12:28:06 -0800

    Update Sprint_1
    - Add TerrainGenerator, class that create vertices, indices and generate mesh with this two properties.
    - Add TerrainData witch is a ScriptableObject to wrap the TerrainGenerator and allow to tune in gameplay mode.
    - Add new GameObject in scene with this TerrainData to show in scene and generate vertices and meshes created.

---
+++ Assets/Editors/TerrainEditor.cs
@@ -0,0 +1,17 @@
+﻿using Terrain;
+using UnityEditor;
+
+[CustomEditor(typeof(TerrainGameObject))]
+public class TerrainEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("Width"));
+        serializedObject.ApplyModifiedProperties();
+
+
+      
+    }
+}
---
+++ Assets/Scripts/TerrainGameObject.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class TerrainGameObject : MonoBehaviour
+{
+    [SerializeField] private TerrainGeneratorSO _terrainData = null;
+    private Mesh _mesh = null;
+
+    private void Awake()
+    {
+        if (!TryGetComponent<MeshFilter>(out var meshFilter))
+        {
+            Debug.LogWarning("[WARNING] -- Fail to load Mesh component.");
+        }
+
+        _mesh = new Mesh();
+        meshFilter.mesh = _mesh;
+
+    }
+
+    [ContextMenu("GenerateMeshes")]
+    public void GenerateMeshes()
+    {
+        if (!_mesh) return;
+
+        _mesh.Clear();
+        _mesh.vertices = _terrainData.GeTerrainMesh.vertices;
+        _mesh.triangles = _terrainData.GeTerrainMesh.indicesVertices;
+        _mesh.Optimize();
+        _mesh.RecalculateNormals();
+    }
+
+    [ContextMenu("ChangeHeightMap")]
+    public void ChangeHeightMap()
+    {
+        _terrainData.SetHeightMap();
+        _terrainData.GenerateShape();
+    }
+
+    private void OnDrawGizmos()
+    {
+        _terrainData.GenerateShape();
+        if (_terrainData.GeTerrainMesh.vertices == null) return;
+
+        Vector3[] vertices = _terrainData.GeTerrainMesh.vertices;
+
+        for (int x = 0; x < vertices.Length; x++)
+        {
+            Gizmos.DrawSphere(vertices[x], 0.1f);
+        }
+    }
+
+}
