1683a956455efab9915e4ac70bb702d45fca3d69
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2021-02-02 14:00:33 -0800

    Update Sprint 2
    - Add URP ( Universal Render Pipeline) to use new shaders convetions from Unity.
    - Add new material shader to check the vertex color;
    - Add more 3 layers for Perlin Noise to make more smooth vertices.
    - Increase the the resolution for mesh quad;
    - Test.

--- Assets/ScriptableObjects/TerrainGeneratorSO.cs
+++ Assets/ScriptableObjects/TerrainGeneratorSO.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TerrainData", menuName = "Terrain/Terrain")]
 public class TerrainGeneratorSO : ScriptableObject
 {
-    [Range(1.0f, 100.0f)]
     public int width = 1;
-    [Range(1.0f, 100.0f)]
     public int height = 1;
-    [Range(1.0f,10.0f)]
     public float heightMap = 1.0f;
+    public float scale = 2.0f;
+
+    public float amplitude_1 = 0.0f;
+    public float amplitude_2 = 0.0f;
+    public float amplitude_3 = 0.0f;
+    public float frenquency_1 = 0.0f;
+    public float frenquency_2 = 0.0f;
+    public float frenquency_3 = 0.0f;
+
+    public Gradient gradientColor;
 
     private TerrainGenerator _terrainGenerator = null;
     private void OnEnable()
@@ -24,13 +31,24 @@ public class TerrainGeneratorSO : ScriptableObject
     public TerrainGenerator.TerrainMesh GeTerrainMesh => _terrainGenerator.TerrainMeshes;
 
     public void SetHeightMap() => _terrainGenerator.HeightMap = heightMap;
-    
+
     public void GenerateShape()
     {
         _terrainGenerator.Width = width;
         _terrainGenerator.Height = height;
+        _terrainGenerator.Scale = scale;
+
+        _terrainGenerator.Amplitude_1 = amplitude_1;
+        _terrainGenerator.Amplitude_2 = amplitude_2;
+        _terrainGenerator.Amplitude_3 = amplitude_3;
+        
+        _terrainGenerator.Frequency_1 = frenquency_1;
+        _terrainGenerator.Frequency_2 = frenquency_2;
+        _terrainGenerator.Frequency_3 = frenquency_3;
+
         SetHeightMap();
         _terrainGenerator.GenerateVertices();
+        _terrainGenerator.GenerateColors(ref gradientColor);
         _terrainGenerator.GenerateIndices();
     }
 
--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -7,12 +7,25 @@ namespace Terrain
     {
         public int Width { get; set; }
         public int Height { get; set; }
+        public float Scale { get; set; }
 
         public float HeightMap { get; set; } = 0.0f;
+
+        public float Amplitude_1 { get; set; } = 0.0f;
+        public float Amplitude_2 { get; set; } = 0.0f;
+        public float Amplitude_3 { get; set; } = 0.0f;
+        public float Frequency_1 { get; set; } = 0.0f;
+        public float Frequency_2 { get; set; } = 0.0f;
+        public float Frequency_3 { get; set; } = 0.0f;
+
+        private float _minTerrainHeight;
+        private float _maxTerrainHeight;
 
         public struct TerrainMesh
         {
             public Vector3[] vertices;
+            public Vector2[] uvs;
+            public Color[] colors;
             public int[] indicesVertices;
         }
 
@@ -21,12 +34,12 @@ namespace Terrain
         public void GenerateVertices()
         {
             TerrainMeshes.vertices = new Vector3[(Width + 1) * (Height + 1)];
+
             for (int i = 0, z = 0; z <= Height; ++z) // rolls
             {
                 for (int x = 0; x <= Width; ++x) // columns
                 {
-                    //TerrainMeshes.vertices[i++] = new Vector3(x, HeightMap, z);
-                    TerrainMeshes.vertices[i++] = new Vector3(x, UnityEngine.Random.Range(0.0f, HeightMap), z);
+                    TerrainMeshes.vertices[i++] = new Vector3(x, Noise(x, z), z);
                 }
             }
         }
@@ -43,10 +56,10 @@ namespace Terrain
                     // |  \   |
                     // |   \  |
                     // lb --- rb
-                    int leftBotton = vertices + 0;      // left botton
-                    int leftTop = vertices + Width + 1; // left top
-                    int rightBotton = vertices + 1;     // right botton
-                    int rightTop = leftTop + 1;         // right top
+                    int leftBotton = vertices + 0;        // left botton
+                    int leftTop = vertices + Width + 1;   // left top
+                    int rightBotton = leftBotton + 1;     // right botton
+                    int rightTop = leftTop + 1;           // right top
 
                     TerrainMeshes.indicesVertices[tris + 0] = leftBotton;
                     TerrainMeshes.indicesVertices[tris + 1] = leftTop;
@@ -61,6 +74,38 @@ namespace Terrain
             }
         }
 
+        public void GenerateColors(ref Gradient gradient)
+        {
+            TerrainMeshes.colors = new Color[TerrainMeshes.vertices.Length];
+            for (int i = 0, z = 0; z <= Height; ++z) // rolls
+            {
+                for (int x = 0; x <= Width; ++x) // columns
+                {
+                    float y = Mathf.InverseLerp(_minTerrainHeight, _maxTerrainHeight, TerrainMeshes.vertices[i].y);
+                    TerrainMeshes.colors[i] = gradient.Evaluate(y);
+                    i++;
+                }
+            }
+        }
+
+        private float Noise(int x, int z)
+        {
+            float decimalProportionX = (float)x / Width;
+            float decimalProportionY = (float)z / Height;
+            //float noise = Mathf.PerlinNoise(decimalProportionX, decimalProportionY);
+            //_maxTerrainHeight = Mathf.Max(_maxTerrainHeight, noise);
+            //_minTerrainHeight = Mathf.Min(_minTerrainHeight, noise);
+            //return noise * Scale;
+
+            float noiseLayers =
+                    Amplitude_1 * Mathf.PerlinNoise(decimalProportionX * Frequency_1, decimalProportionY * Frequency_1)
+                    + Amplitude_2 * Mathf.PerlinNoise(decimalProportionX * Frequency_2, decimalProportionY * Frequency_2)
+                    + Amplitude_3 * Mathf.PerlinNoise(decimalProportionX * Frequency_3, decimalProportionY * Frequency_3)
+                        * Scale;
+            _maxTerrainHeight = Mathf.Max(_maxTerrainHeight, noiseLayers);
+            _minTerrainHeight = Mathf.Min(_minTerrainHeight, noiseLayers);
+            return noiseLayers;
+        }
     }
 
 }
